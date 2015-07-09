f9405f3f4dfe0aed775ad04e8bcfa3bd415f93ba
Blake Johnson <blake41@gmail.com>
2015-07-09 14:44:47 -0400

    edits

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var app = {};
 function bootstrap() {
   // instantiate your view object and append it's HTML to the DOM.
-  // instantiate your model object
-  // pass the model object to the view when you instantiate it
-  attach your view object to the global app object so we have access to it anywhere. 
+  // attach your view object to the global app object so we have access to it anywhere. 
 }
