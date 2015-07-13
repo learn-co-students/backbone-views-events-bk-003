1c17025322ecf6aba5f11205b6b5f636221fa129
Katie Hoffman <ktahoffman@gmail.com>
2015-07-13 12:42:57 -0400

    enhance comments

--- views/main.js
+++ views/main.js
@@ -1,1 +1,11 @@
-// create your view object here.  It should extend (inherit) from the main Backbone view object.
+// Create your view object here:
+
+//  * It should extend (inherit) from the main Backbone view object
+
+//  * It should have a property called render:
+//    * The render function should append the following HTML:
+//      <a href="/whatever">to the internet</a>
+
+//  * It should have a property called handleClick which points to a function
+//    * The handleClick function should get called when someone clicks on a link
+//      and then log the string "something" to the console
