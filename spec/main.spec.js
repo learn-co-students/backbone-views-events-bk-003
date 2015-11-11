524141e5064cfb68a5622cd1606d8987df336e94
BenjaGross <benja.j.gross@gmail.com>
2015-11-11 14:37:19 -0500

    updates spec from solution

--- spec/main.spec.js
+++ spec/main.spec.js
@@ -1,11 +1,19 @@
 'use strict';
 
-describe( "#bootstrap", function() {
-  it("should handle a click on the a tag by delegating to a handleClick function on your view", function() {
-    var spy = spyOn(MainView.prototype, "handleClick")
+describe( "#boostrap", function() {
+  var spy;
+  it("should instantiate your view and return an object", function() {
+    spy = spyOn(MainView.prototype, "handleClick");
     bootstrap();
-    expect($("body")).toContainHtml('<a href="/whatever">to the internet</a>');
+    expect(app.mainView.el).toContainHtml("<a href='/whatever'>to the internet</a>")
+  });
+
+  it("should append the proper html string to the body", function() {
+    expect($("body")).toContainHtml("<a href='/whatever'>to the internet</a>");
+  });
+
+  it("should handle a click on the a tag by delegating to a #handleClick function on your view", function() {
     $("a").click()
     expect(spy).toHaveBeenCalled();
-  });    
+  });  
 });
