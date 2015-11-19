81ac600a534c8679829cf5c9377523981801c9a7
jmdelvalle <jeremy.delvalle@flatironschool.com>
2015-11-19 10:10:52 -0500

    completed

--- views/main.js
+++ views/main.js
@@ -9,3 +9,19 @@
 //  * It should have a property called handleClick which points to a function
 //  * The handleClick function should get called when someone clicks on a link
 //  * and then log the string "something" to the console
+
+MainView = Backbone.View.extend({
+  events: {
+    "click a": "handleClick"
+  },
+  initialize : function (){
+    this.render();
+  },
+  render: function() {
+    this.$el.append("<a href='/whatever'>to the internet</a>");
+    return this;
+  },
+  handleClick: function(){
+    console.log("something");
+  }
+});
