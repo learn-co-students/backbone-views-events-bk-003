594f3fbad5290b0fe34fb8f08a7a4f37827ac3de
cloudleo <l.castillo1989@hotmail.com>
2015-11-20 02:03:58 -0500

    dope

--- app.js
+++ app.js
@@ -2,4 +2,10 @@ var app = {};
 function bootstrap() {
   // instantiate your view object and append it's HTML to the DOM.
   // attach your view object to the global app object so we have access to it anywhere. 
+
+var view = new MainView() ;
+app.mainView = view ;
+$('body').append(app.mainView.el) ;
+
+
 }
