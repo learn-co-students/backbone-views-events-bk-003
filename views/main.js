2cb224c5ad03d85a5ac72588b4befe29377c6e3c
djoseph1250 <djoseph1250@gmail.com>
2015-11-20 15:24:22 -0500

    solved

--- views/main.js
+++ views/main.js
@@ -1,5 +1,23 @@
 // Create your view object here:
-
+MainView = Backbone.View.extend({
+	initialize : function() {
+		this.render();
+		this.handleClick();
+	},
+	render : function() {
+		var html = '<a href="/whatever">to the internet</a>';
+		this.$el.append(html);
+		return this;
+	},
+	handleClick : function() {
+		// if (true) {
+		// 	console.log('something');
+		// };
+		$('a').click(function() {
+			console.log('something');
+		});
+	}
+});
 //  * It should extend (inherit) from the main Backbone view object
 
 //  * It should have a property called render:
