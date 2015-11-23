2a4c6e4e7b157826f210338cc333d479b6487b81
bmesa620 <bmesa620@gmail.com>
2015-11-22 21:46:30 -0500

    woohoo

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-var app = {};
-function bootstrap() {
+app = {};
+	function bootstrap() {
   // instantiate your view object and append it's HTML to the DOM.
+  		app.mainView = new MainView();
   // attach your view object to the global app object so we have access to it anywhere. 
+		$('body').append(app.mainView.el);
 }
