42df357f7142540d6608241bd40458fd635f36c4
RaptorDog <randycastro20@gmail.com>
2015-11-24 15:35:00 -0500

    YUSSSS

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var app = {};
 function bootstrap() {
+  app.mainView = new MainView
   // instantiate your view object and append it's HTML to the DOM.
   // attach your view object to the global app object so we have access to it anywhere. 
+  $("body").append(app.mainView.el)  
 }
--- views/main.js
+++ views/main.js
@@ -1,4 +1,19 @@
 // Create your view object here:
+var MainView = Backbone.View.extend({
+  events: {
+    "click a": "handleClick" //"click #link1"
+  },
+  initialize: function(){
+    this.render();
+  },
+  render: function(){
+    this.$el.append('<a href="/whatever">to the internet</a>') // id="link1" as id tag for click event
+  },
+  handleClick: function (){
+   
+    console.log("something");
+  }
+});x
 
 //  * It should extend (inherit) from the main Backbone view object
 
