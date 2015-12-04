c282a37a515f9e381808b65c13d9dc47ca964dd4
Giancarlo Martinez <g.r.rmartinez49@gmail.com>
2015-12-04 11:49:15 -0500

    add main view with click handler and initialize in app

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var app = {};
 function bootstrap() {
-  // instantiate your view object and append it's HTML to the DOM.
-  // attach your view object to the global app object so we have access to it anywhere. 
+  	app.mainView = new MainView;
+	$('body').append(app.mainView.el);
 }
